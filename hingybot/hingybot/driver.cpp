fd9a57370a2dd93c97c18a5633f8847a5cd7a661
SP_CeZaR <cezare22@gmail.com>
2017-07-27 15:01:10 +0200

    Tester works on Windows

--- hingybot/hingybot/driver.cpp
+++ hingybot/hingybot/driver.cpp
@@ -17,19 +17,19 @@ HingyDriver::HingyDriver(stringmap params) : Driver(params)
 
     speed_factor = std::stof(params["speed_factor"]);
     speed_base = std::stof(params["speed_base"]);
-    
+
     int hinges_iterations = atoi(params["hinges_iterations"].c_str());
 
-    if (gui) 
+    if (gui)
         track = std::make_shared<HingyTrackGui>(params["track"], 1000, 1000);
     else
         track = std::make_shared<HingyTrack>(params["track"]);
 
     if (!record) {
-	if(!file_exists(params["track"])){
-	    log_error("Couldn't load the track!");
-	    throw;
-	}
+        if (!file_exists(params["track"])) {
+            log_error("Couldn't load the track!");
+            throw;
+        }
 
         track->ConstructBounds();
         track->ConstructHinges(13);
@@ -47,7 +47,8 @@ HingyDriver::HingyDriver(stringmap params) : Driver(params)
         track->ConstructSpeeds(sa, sb, sc);
         if (gui)
             std::static_pointer_cast<HingyTrackGui>(track)->TickGraphics();
-    } else {
+    }
+    else {
         track->BeginRecording();
     }
 
@@ -88,20 +89,20 @@ void HingyDriver::Cycle(CarSteers& steers, const CarState& state)
     float target_speed;
 
     if(track->Recording())
-	target_speed = 45.0f;
+    target_speed = 45.0f;
     else {
-	float hs = track->GetHingeSpeed();
-	if (hs < 1.0f)
-	    target_speed = hs * speed_factor + speed_base;
-	else
-	    target_speed = 1000.0f;
+    float hs = track->GetHingeSpeed();
+    if (hs < 1.0f)
+        target_speed = hs * speed_factor + speed_base;
+    else
+        target_speed = 1000.0f;
     }
 
     steers.hand_brake = std::max(-steers.gas, 0.0f);
     steers.gas = (target_speed - state.speed_x) / 35.0f;
 
     if(std::abs(state.cross_position) > 1.0f)
-	steers.gas = 0.0f;
+    steers.gas = 0.0f;
 
     SetClutchAndGear(state, steers);
 }
@@ -112,10 +113,10 @@ void HingyDriver::SetClutchAndGear(const CarState & state, CarSteers & steers)
     
     if (state.rpm > 9000.0f){
         steers.clutch = 0.5f;
-	gear_dir = 1;
+    gear_dir = 1;
     } else if (state.rpm < 5000.0f && last_rpm >= 5000.0f) {
-	steers.clutch = 0.5f;
-	gear_dir = -1;
+    steers.clutch = 0.5f;
+    gear_dir = -1;
     }	
     
     if (steers.clutch > 0.4f) {
@@ -124,7 +125,7 @@ void HingyDriver::SetClutchAndGear(const CarState & state, CarSteers & steers)
     }
 
     if(state.speed_x < 30.0f && steers.gas > 0.4f)
-	steers.gear = 1;
+    steers.gear = 1;
 
     steers.clutch = std::max(0.0f, steers.clutch - 0.1f);
     last_rpm = state.rpm;
